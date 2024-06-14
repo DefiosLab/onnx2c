71dbdea5a241f196f1879cb43a6dbd81048c4049
Taiki Kondo <kontaiml@gmail.com>
2024-06-15 06:36:45 +0900

    format

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from setuptools import setup,
-    find_packages
+from setuptools import setup, find_packages
 
-        setup(name = "onnx2c", version = "0.1.0", packages = find_packages(),
-              entry_points = {
-                "console_scripts" : [
-                  "onnx2c=onnx2c.onnx2c:main",
-                ],
-              },
-              install_requires = [
-                "numpy",
-                "onnxruntime",
-                "scipy",
-                "onnx",
-                "onnx-simplifier",
-                "onnxconverter-common",
-              ], )
+setup(
+    name="onnx2c",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "onnx2c=onnx2c.onnx2c:main",
+        ],
+    },
+    install_requires=[
+        "numpy",
+        "onnxruntime",
+        "scipy",
+        "onnx",
+        "onnx-simplifier",
+        "onnxconverter-common",
+    ],
+)
