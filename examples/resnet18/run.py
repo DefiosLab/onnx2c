b44463d65b4d68981b6252f0efb66c05fcb6936b
Taiki Kondo <kontaiml@gmail.com>
2024-06-15 06:22:05 +0900

    add example resnet

--- examples/resnet18/run.py
+++ examples/resnet18/run.py
@@ -7,14 +7,36 @@ import time
 import sys
 import os
 import ctypes
+import urllib
+import json
+
+def download_imagenet_labels(filename="imagenet_labels.json"):
+    url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            labels = json.load(f)
+    else:
+        with urllib.request.urlopen(url) as response:
+            labels = json.loads(response.read())
+            # ラベルをローカルファイルに保存
+            with open(filename, "w") as f:
+                json.dump(labels, f)
+
+    return labels
+
 
 print("Loading model...", end="")
-model_path = "bn.onnx"
+model_path = "resnet18-v1-7.onnx"
 model = onnx.load(model_path)
 print("Done")
+img = cv2.imread("cat.jpg")
+img = img.transpose(2,0,1)
+mean = np.array([123.675, 116.28, 103.53]).reshape(1,3,1,1)
+std = np.array([58.395, 57.12, 57.375]).reshape(1,3,1,1)
+img = (img - mean)/std
+img =  np.ascontiguousarray(img.astype(np.float32))
 
-img = np.random.rand(1,3,224,224).astype(np.float32)
-out = np.zeros((1,64,112,112)).astype(np.float32)
+out = np.zeros((1, 1000)).astype(np.float32)
 
 # onnxruntime
 ort_sess = onnxruntime.InferenceSession(model_path)
@@ -44,8 +66,9 @@ c_func(input_c, output_c)
 ed = time.time()
 vfv_time = ed - st
 
-
-print("Predicted label : {}".format(np.argmax(result_vfv)))
+imagenet_labels = download_imagenet_labels()
+pred = imagenet_labels[np.argmax(result_vfv)]
+print("Predicted label : {}".format(pred))
 print("onnxruntime time:{:.2f}sec".format(ort_time))
 print("C Lang time:{:.2f}sec".format(vfv_time))
 
--- examples/resnet18/wrapper.c
+++ examples/resnet18/wrapper.c
@@ -3,6 +3,6 @@
 #include "inference.h"
 void run(float* input, float* output){
   float_tensor inp = {4,{1,3,224,224},input};
-  float_tensor out = {2,{1,64,112,112},output};
+  float_tensor out = {2,{1,1000},output};
   inference(&inp,&out);
 }
